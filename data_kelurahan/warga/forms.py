2ebc88fd8bd28d0a7c80fc502bfeebcbdcc1d8ad
MRYDXXZ <riomario090305@gmail.com>
2025-10-15 19:28:12 +0700

    commit pertemuan 3

---
+++ data_kelurahan/warga/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Warga, Pengaduan
+
+
+class WargaForm(forms.ModelForm):
+    class Meta:
+        model = Warga
+        # Tentukan field mana saja dari model yang ingin ditampilkan di form
+        fields = ['nik', 'nama_lengkap', 'alamat', 'no_telepon']
+
+
+class PengaduanForm(forms.ModelForm):
+    class Meta:
+        model = Pengaduan
+        # Include fields: judul, isi, status, pelapor
+        fields = ['judul', 'isi', 'status', 'pelapor']
